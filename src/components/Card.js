b0a893f53cbd16905872b51cad16788eac803e65
Azar-Pav <exneps@gmail.com>
2023-07-13 14:14:08 +0300

    feat: контекст в card, скрытие удаления

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { CurrentUserContext } from '../contexts/CurrentUserContext';
 
 function Card({card, onCardClick}) {
+
+  const currentUser = useContext(CurrentUserContext);
+  const isOwn = card.owner._id === currentUser._id;
+  const isLiked = card.likes.some(i => i._id === currentUser._id);
+
+  const cardLikeButtonClassName = (
+    `elements__like ${isLiked && 'elements__like_active'}`
+  );
 
   function handleClick() {
     onCardClick(card);
@@ -8,20 +17,23 @@ function Card({card, onCardClick}) {
 
   return (
     <li className="elements__element">
-      <button
-        className="elements__delete"
-        aria-label="Удалить"
-        type="button"
-      ></button>
+      {isOwn &&
+        <button
+          className="elements__delete"
+          aria-label="Удалить"
+          type="button"
+          /* onClick={handleDeleteClick} */
+        />
+      }
       <img src={card.link} alt={card.name} className="elements__image" onClick={handleClick} />
       <div className="elements__items">
         <h2 className="elements__text">{card.name}</h2>
         <div className="elements__like-container">
           <button
-            className="elements__like"
+            className={cardLikeButtonClassName}
             aria-label="Нравится"
             type="button"
-          ></button>
+          />
           <p className="elements__like-counter">{card.likes.length}</p>
         </div>
       </div>
